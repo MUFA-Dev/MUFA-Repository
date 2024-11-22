8d992d6c6ba2454eebd504befb2382207b1c3a00
gtoutzia <gtoutzia@ece.auth.gr>
2024-11-22 20:47:24 +0200

    update

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-'use strict';
+// 'use strict';
 
-var path = require('path');
-var http = require('http');
-var oas3Tools = require('oas3-tools');
-var serverPort = process.env.PORT || 8080;
+// var path = require('path');
+// var http = require('http');
+// var oas3Tools = require('oas3-tools');
+// var serverPort = process.env.PORT || 8080;
 
-// swaggerRouter configuration
-var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
-};
+// // swaggerRouter configuration
+// var options = {
+//     routing: {
+//         controllers: path.join(__dirname, './controllers')
+//     },
+// };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+// var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+// var app = expressAppConfig.getApp();
 
-// Define a simple route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-});
-//-------------------------------------------------------
-// const express = require('express');
-// const app = express();
-
-// // Use the environment's PORT variable or default to 3000
-// const PORT = process.env.PORT || 3000;
-
+// // Define a simple route for the root URL
 // app.get('/', (req, res) => {
 //   res.send('Hello, world!');
 // });
 
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
+// // Initialize the Swagger middleware
+// http.createServer(app).listen(serverPort, function () {
+//     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+//     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 // });
+//-------------------------------------------------------
+const express = require('express');
+const app = express();
+
+// Use the environment's PORT variable or default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 //-------------------------------------------------------
 // import "dotenv/config";
 
