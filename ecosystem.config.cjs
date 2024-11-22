bdbb747a105251b58e5743eb38efd33cc7c14216
gtoutzia <gtoutzia@ece.auth.gr>
2024-11-22 18:43:29 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
 	apps: [
 		{
-			name: "server",
+			name: "MUFA_backend",
 			script: "npm",
 			args: "start",
 			env: {
 				NODE_ENV: "production",
-				PORT: 4000,
+				PORT: 3000,
 			},
 		},
 	],
