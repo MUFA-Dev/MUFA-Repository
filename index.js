c8e436df39ccc45409a347cdd28a1f00147ff488
gtoutzia <153185439+gtoutzia@users.noreply.github.com>
2024-11-22 15:22:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,35 +16,11 @@ var options = {
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// // Initialize the Swagger middleware
-// http.createServer(app).listen(serverPort, function () {
-//     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-//     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-// });
-
- const express = require('express');
- const app = express();
-
- //Use the environment's PORT variable or default to 3000
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-=======
-=======
->>>>>>> parent of a9f4e39 (Update index.js)
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-<<<<<<< HEAD
->>>>>>> parent of a9f4e39 (Update index.js)
-=======
->>>>>>> parent of a9f4e39 (Update index.js)
 });
-
 // const express = require('express');
 // const app = express();
 
