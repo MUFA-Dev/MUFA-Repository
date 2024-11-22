b9a80dab1ccd71f9d9987ecec54eb852c8d9463e
sokiang <164039233+sokiang@users.noreply.github.com>
2024-11-22 21:05:44 +0200

    Update index.js

--- nodejs-server-server-generated/index.js
+++ nodejs-server-server-generated/index.js
@@ -2,7 +2,6 @@
 
 var path = require('path');
 var http = require('http');
-
 var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
 
@@ -16,7 +15,17 @@ var options = {
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
 
-// Initialize the Swagger middleware
+// Custom route για το root
+app.get('/', (req, res) => {
+    res.status(200).send('It works!');
+});
+
+// Διόρθωση για το deprecation warning του body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Start the HTTP server
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
