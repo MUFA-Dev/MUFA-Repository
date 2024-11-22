de95cf5ac4c9fdb7916742d4adbe745b3c20ccdb
gtoutzia <153185439+gtoutzia@users.noreply.github.com>
2024-11-22 15:27:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-'use strict';
+// 'use strict';
 
-var path = require('path');
-var http = require('http');
+// var path = require('path');
+// var http = require('http');
 
-var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
+// var oas3Tools = require('oas3-tools');
+// var serverPort = 8080;
 
-// swaggerRouter configuration
-var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
-};
+// // swaggerRouter configuration
+// var options = {
+//     routing: {
+//         controllers: path.join(__dirname, './controllers')
+//     },
+// };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+// var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+// var app = expressAppConfig.getApp();
 
-// Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+// // Initialize the Swagger middleware
+// http.createServer(app).listen(serverPort, function () {
+//     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+//     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+// });
+const express = require('express');
+const app = express();
+
+// Use the environment's PORT variable or default to 3000
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
-// const express = require('express');
-// const app = express();
 
-// // Use the environment's PORT variable or default to 3000
-// const PORT = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, world!');
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
