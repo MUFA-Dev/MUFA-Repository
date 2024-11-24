54811e82de82dc27ffca2a5ca4cac4ebcaad605e
gtoutzia <153185439+gtoutzia@users.noreply.github.com>
2024-11-24 15:45:51 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,13 @@
-module.exports = {
-    apps: [
-        {
-            name: "MUFA_backend",
-            script: "npm",
-            args: "start",
-            env: {
-                NODE_ENV: "production",
-                PORT: process.env.PORT || 3000,
-            },
-        },
-    ],
-};
+// module.exports = {
+//     apps: [
+//         {
+//             name: "MUFA_backend",
+//             script: "npm",
+//             args: "start",
+//             env: {
+//                 NODE_ENV: "production",
+//                 PORT: process.env.PORT || 3000,
+//             },
+//         },
+//     ],
+// };
