b687a14bf3e1b0075a484920cfcdfdca13de79c9
gtoutzia <gtoutzia@ece.auth.gr>
2024-11-27 16:12:41 +0200

    Changes in init.test,js and package.json

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,23 +1,44 @@
-import http from "node:http";
-
-import test from "ava";
-import got from "got";
-
-import app from "../index.js";
+import { createServer } from 'http';
+import test from 'ava';
+import got from 'got';
+import app from '../index.js';
 
 test.before(async (t) => {
-	t.context.server = http.createServer(app);
+    t.context.server = createServer(app);
     const server = t.context.server.listen();
     const { port } = server.address();
-	t.context.got = got.extend({ responseType: "json", prefixUrl: `http://localhost:${port}` });
+    t.context.got = got.extend({ responseType: 'json', prefixUrl: `http://localhost:${port}` });
 });
 
 test.after.always((t) => {
-	t.context.server.close();
+    t.context.server.close();
 });
 
-test("GET /api returns correct response and status code", async (t) => {
-	const { body, statusCode } = await t.context.got("api");
-	t.is(body.message, "It works!");
-	t.is(statusCode, 200);
+test('GET /api returns correct response and status code', async (t) => {
+    const { body, statusCode } = await t.context.got('api');
+    t.is(body.message, 'It works!');
+    t.is(statusCode, 200);
 });
+// import http from "node:http";
+
+// import test from "ava";
+// import got from "got";
+
+// import app from "../index.js";
+
+// test.before(async (t) => {
+// 	t.context.server = http.createServer(app);
+//     const server = t.context.server.listen();
+//     const { port } = server.address();
+// 	t.context.got = got.extend({ responseType: "json", prefixUrl: `http://localhost:${port}` });
+// });
+
+// test.after.always((t) => {
+// 	t.context.server.close();
+// });
+
+// test("GET /api returns correct response and status code", async (t) => {
+// 	const { body, statusCode } = await t.context.got("api");
+// 	t.is(body.message, "It works!");
+// 	t.is(statusCode, 200);
+// });
