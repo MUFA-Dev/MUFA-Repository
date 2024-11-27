0ebd742cbd90f765b277b80e75dbade98d3d9bef
gtoutzia <gtoutzia@ece.auth.gr>
2024-11-27 16:23:22 +0200

    changes

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
+
+export default app;
+
 //-------------------------------------------------------
 // const express = require('express');
 // const app = express();
