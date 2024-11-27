ec683436b6db1412a8f88a30b6de755055798df1
gtoutzia <gtoutzia@ece.auth.gr>
2024-11-27 16:28:47 +0200

    many changes

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 'use strict';
 
-var path = require('path');
-var http = require('http');
+import path from 'path';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import oas3Tools from 'oas3-tools';
 
-var oas3Tools = require('oas3-tools');
-var serverPort = process.env.PORT || 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const serverPort = process.env.PORT || 8080;
 
 // swaggerRouter configuration
-var options = {
+const options = {
     routing: {
         controllers: path.join(__dirname, './controllers')
     },
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+const expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+const app = expressAppConfig.getApp();
+
+// Define a simple route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
+createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
 
 export default app;
+//-------------------------------------------------------
+// 'use strict';
+
+// var path = require('path');
+// var http = require('http');
+
+// var oas3Tools = require('oas3-tools');
+// var serverPort = process.env.PORT || 8080;
+
+// // swaggerRouter configuration
+// var options = {
+//     routing: {
+//         controllers: path.join(__dirname, './controllers')
+//     },
+// };
+
+// var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+// var app = expressAppConfig.getApp();
+
+// // Initialize the Swagger middleware
+// http.createServer(app).listen(serverPort, function () {
+//     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+//     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+// });
+
+// export default app;
 
 //-------------------------------------------------------
 // const express = require('express');
