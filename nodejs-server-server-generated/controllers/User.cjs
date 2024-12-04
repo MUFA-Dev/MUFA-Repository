5d3f7db6f24238c1b4c8714f5bf828a5c739f714
alexkrit02 <alexkrit@ece.auth.gr>
2024-12-04 22:18:25 +0200

    testing

---
+++ nodejs-server-server-generated/controllers/User.cjs
@@ -0,0 +1,14 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Default = require('../service/DefaultService.js');
+
+module.exports.userUser_idPostPUT = function userUser_idPostPUT(req, res, next, body, song_lyrics, song_album_cover, song_canvas, user_id) {
+    Default.userUser_idPostPUT(body, song_lyrics, song_album_cover, song_canvas, user_id)
+        .then(function(response) {
+            utils.writeJson(res, response);
+        })
+        .catch(function(response) {
+            utils.writeJson(res, response);
+        });
+};
