120715e2b06b2e584388fad58d1095020dc95400
gtoutzia <gtoutzia@ece.auth.gr>
2024-12-08 16:31:27 +0200

    commit changes

---
+++ cypress/e2e/init.cy.js
@@ -0,0 +1,49 @@
+describe('login', () => {
+	beforeEach(() => {
+		cy.visit('https://mufa-repository-1.onrender.com/docs/#/')
+	})
+
+it("Ensure that homepage loads", () => {
+    cy.get(".swagger-ui").should("exist")
+
+})
+
+
+it("Click on first endpoint and verify responses", () => {
+  // Check that clicking the endpoint works
+  cy.get('#operations-default-userUser_idSongGET').click();
+
+  // Check that 'Try it out' button works
+  cy.get('.try-out').click();
+
+  // Type something in the user_id placeholder
+  cy.get('input[placeholder="user_id"]').type('1');
+
+  // Click the 'Execute' button
+  cy.get('.opblock-control__btn').click();
+
+  // Check that a response is given (non-zero length)
+  cy.get('.content-type').should('have.length.greaterThan', 0);
+
+  // Verify the request URL
+  cy.get('.request-url').should(
+    'contain.text',
+    'https://mufa-repository-1.onrender.com/user/1/song'
+  );
+
+  // Check the response codes (wait for them to appear)
+  cy.get('.response-col_status').should('be.visible');
+  //Ensure that responses 500,400 and 200 are present
+  cy.get('.response-col_status').contains('500');
+  cy.get('.response-col_status').contains('400');
+  cy.get('.response-col_status').contains('200');
+  // Verify the download button works
+  cy.get('.download-contents').should('be.visible');
+  //verify that download button works
+    //cy.get('.download-contents').click();
+  //Verify that media type button works
+  cy.get('.content-type-wrapper ').click();
+  cy.get('.content-type').should('be.visible');
+})
+
+})
