7c4b39c894088a124f5199e0e86632f7276b655c
gtoutzia <gtoutzia@ece.auth.gr>
2024-12-14 19:56:39 +0200

    final commit

--- controllers/Default.js
+++ controllers/Default.js
@@ -95,3 +95,33 @@ module.exports.userUser_idFollowingFollowing_idPUT = function userUser_idFollowi
       });
   };
   
+  //PUT user/{user_id}/following/{following_id}/post/{post_id}/song/{song_id}
+  module.exports.userUser_idFollowingFollowing_idPostPost_idSongSong_idPUT = function userUser_idFollowingFollowing_idPostPost_idSongSong_idPUT (req, res, next, user_id, following_id, post_id, song_id) {
+     Default.userUser_idFollowingFollowing_idPostPost_idSongSong_idPUT(user_id, following_id, post_id, song_id)
+      .then(function (response) {
+        utils.writeJson(res,response,response.code);
+      })
+      .catch(function (err) {
+        utils.writeJson(res,err,err.code);
+      });
+  };
+  //PUT user/{user_id}/following/{following_id}/post/{post_id}
+  module.exports.userUser_idFollowingFollowing_idPostPost_idPUT = function userUser_idFollowingFollowing_idPostPost_idPUT (req, res, next, user_id, following_id, post_id, like, comment, report) {
+    Default.userUser_idFollowingFollowing_idPostPost_idPUT(user_id, following_id, post_id, like, comment, report)
+      .then(function (response) {
+        utils.writeJson(res,response,response.code);
+      })
+      .catch(function (err) {
+        utils.writeJson(res,err,err.code);
+      });
+  };
+  //DEL user/{id}/post/{id}
+  module.exports.userUser_idPostPost_idDELETE = function userUser_idPostPost_idDELETE (req, res, next, user_id, post_id) {
+     Default.userUser_idPostPost_idDELETE(user_id, post_id)
+      .then(function (response) {
+        utils.writeJson(res,response,response.code);
+      })
+      .catch(function (err) {
+        utils.writeJson(res,err,err.code);
+      });
+  };
--- tests/init.test.mjs
+++ tests/init.test.mjs
@@ -41,7 +41,7 @@ test("GET/user/{user_id}/notifications/posts returns correct response and status
 });
 
 test("GET/user/{id}/song with correct inputs", async (t) => {
-  const response= await t.context.got('user/1/song?song_name=Nyxterides');
+  const response= await t.context.got('user/1/song?song_name=Nyxterides', {throwHttpErrors: false});
   t.is(response.statusCode, 200);
   console.log(response.statusCode,response.body);
 });
@@ -408,4 +408,145 @@ test("PUT /user/{user_id}/spotify with non-existent user_id", async (t) => {
   });
   t.is(statusCode, 400);
   t.is(body.message, "User not found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} - Successfull Interaction like & comment & report", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3?like=true&comment=Loveit&report=false',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Post interaction successfull.");
+  t.is(body.body.likes, 1); // Should increment by 1
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Successfull Interaction Like ", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3?like=true',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Post interaction successfull.");
+  t.is(body.body.likes, 1); // Should increment by 1
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Successfull interaction Like", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3?like=false',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Post interaction successfull.");
+  t.is(body.body.likes, 0); // Should remain the same
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Successfull interaction comment", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3?comment=Loveit',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Post interaction successfull.");
+  t.is(body.body.comments[0], "Loveit"); // Should append new comment to comments
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Successfull Interaction report", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3?report=true',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Post interaction successfull.");
+  t.is(body.body.reports, 1); // Should increment by 1
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Invalid userId ", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/9999999999/following/100/post/3',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "User Not Found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Invalid postId ", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/99999999999',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "Post not found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id} -Invalid followingId ", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/99999999/post/3',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "Post not found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id}/song/{id} - Add a song to spotify successfully", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3/song/1',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Song has been added to Spotify.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id}/song/{id} - User not found", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/99999999/following/100/post/3/song/1',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "User Not Found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id}/song/{id} -Following not found", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/9999999999999/post/3/song/1',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "Post not found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id}/song/{id} - Song not found", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/3/song/999999999',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "Song not found.");
+});
+
+test("PUT /user/{id}/following/{id}/post/{id}/song/{id} -Post not found", async (t) => {
+  const { body, statusCode } = await t.context.got.put('user/1/following/100/post/9999999999/song/1',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "Post not found.");
+});
+
+test("DEL user/{id}/post/{id} - Delete a post successfully with correct user_Id", async (t) => {
+  const { body, statusCode } = await t.context.got.delete('user/1/post/3',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 200);
+  t.is(body.message, "Post has been deleted successfully.");
+});
+
+test("DEL user/{id}/post/{id} - Delete a post unsuccessfully with incorrect user_Id, correct post_id", async (t) => {
+  const { body, statusCode } = await t.context.got.delete('user/99999999999/post/3',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "User not found.");
+});
+
+test("DEL user/{id}/post/{id} - Delete a post unsuccessfully with correct user_Id, incorrect post_id", async (t) => {
+  const { body, statusCode } = await t.context.got.delete('user/1/post/9999999999',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 404);
+  t.is(body.error, "Post not found.");
+});
+
+test("DEL user/{id}/post/{id} - Delete a post unsuccessfully because user_id and post_id did not exist in the same post", async (t) => {
+  const { body, statusCode } = await t.context.got.delete('user/1/post/10',
+    {throwHttpErrors: false});
+  console.log(body);
+  t.is(statusCode, 400);
+  t.is(body.error, "User does not own the post.");
 });
